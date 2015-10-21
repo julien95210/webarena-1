f8c48418f12c2790ac70f1e10afdec116c0726b7
Anne Flo <luong@edu.ece.fr>
2015-10-21 16:34:08 +0200

    Ajout de commentaires + appels de models

--- Controller/ArenasController.php
+++ Controller/ArenasController.php
@@ -1,24 +1,33 @@
 <?php 
+//Controller Arenas
 
 App::uses('AppController', 'Controller');
 
+
 class ArenasController extends AppController
 	{
+		//Accès à d'autres models que Arenas (Player, Fighter, Event)
+		public $uses = array('Player', 'Fighter', 'Event');
+
+		//Page d'accueil de WebArena
 	    public function index()
 	    {
 	        die('page index');
 	    }
 
+	    //Affichage des informations du fighter
 	    public function fighter()
 	    {
 	        die('page fighter');
 	    }
 
+	    //Affichage les derniers events du player
 	    public function diary()
 	    {
 	        die('page diary');
 	    }
 
+	    //Affichage des actions possibles du fighter (doMove, doAttack)
 	    public function sight()
 	    {
 	        die('page sight');
