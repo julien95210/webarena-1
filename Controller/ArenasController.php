0a53c9c4fc3003c6147c3b34da6ec0cdbdf38277
Anne Flo <luong@edu.ece.fr>
2015-10-21 16:05:51 +0200

    Création Controller Arenas

---
+++ Controller/ArenasController.php
@@ -0,0 +1,13 @@
+<?php 
+
+App::uses('AppController', 'Controller');
+
+class ArenasController extends AppController
+	{
+	    public function index()
+	    {
+	        die('test');
+	    }
+
+	}
+?>
