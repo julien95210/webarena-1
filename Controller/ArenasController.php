7ee1a537b75cd0bb3333c672e6854b756076f0f5
Anne Flo <luong@edu.ece.fr>
2015-10-21 16:08:18 +0200

    Ajout des actions index, fighter, diary & sight

--- Controller/ArenasController.php
+++ Controller/ArenasController.php
@@ -6,8 +6,22 @@ class ArenasController extends AppController
 	{
 	    public function index()
 	    {
-	        die('test');
+	        die('page index');
 	    }
 
+	    public function fighter()
+	    {
+	        die('page fighter');
+	    }
+
+	    public function diary()
+	    {
+	        die('page diary');
+	    }
+
+	    public function sight()
+	    {
+	        die('page sight');
+	    }
 	}
 ?>
